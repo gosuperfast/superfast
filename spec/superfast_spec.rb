79c85b55252556fe3c02579a8bd0229a66f5cea5
Anudeep <154791428+deepgits@users.noreply.github.com>
2024-12-07 13:57:22 +0530

    Create superfast_spec.rb

---
+++ spec/superfast_spec.rb
@@ -0,0 +1,10 @@
+# spec/superfast_spec.rb
+RSpec.describe Superfast do
+  it "has a version number" do
+    expect(Superfast::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
