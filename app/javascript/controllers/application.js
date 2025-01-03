46ff7aef82e2ccc48569ad1026130bae50689140
Zil Norvilis <zilasino27@gmail.com>
2025-01-03 15:43:29 +0200

    Update flash notification

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
+
+import { Alert } from "tailwindcss-stimulus-components";
+application.register("alert", Alert);
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
