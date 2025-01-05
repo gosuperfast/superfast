3a523fd60901331a71e8165f0511f8761322f411
ChunKitJonathanChan <kitcck214@gmail.com>
2025-01-05 04:37:46 -0800

    .

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
-pin '@hotwired/strada'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/strada"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin 'tailwindcss-stimulus-components' # @6.1.2
+pin "tailwindcss-stimulus-components" # @6.1.2
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
 
   namespace :app do
-    get 'dashboard/index'
+    get "dashboard/index"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -19,13 +19,13 @@ Rails.application.routes.draw do
 
   # Public facing site
   # Defines the root path route ("/")
-  root 'home#index'
+  root "home#index"
 
   # Other pages
-  get 'product', to: 'home#product'
+  get "product", to: "home#product"
 
   # Private app
   constraints subdomain: /.+/ do
-    get 'dashboard', to: 'app/dashboard#index', as: :dashboard
+    get "dashboard", to: "app/dashboard#index", as: :dashboard
   end
 end
