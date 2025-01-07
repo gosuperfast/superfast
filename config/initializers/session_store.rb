dc526a0e2baefaac8959ba7411cb87c425d32e04
ChunKitJonathanChan <kitcck214@gmail.com>
2025-01-07 05:00:42 -0800

    add App button in topnav bar after user sign in

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+# config/initializers/session_store.rb
+Rails.application.config.session_store :cookie_store,
+                                       key: "_superfast_session",
+                                       domain: :all
