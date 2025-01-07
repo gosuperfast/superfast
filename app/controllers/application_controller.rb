d82019ef31057751e100bdd69eaad15b57ccbe00
ChunKitJonathanChan <kitcck214@gmail.com>
2025-01-06 22:30:02 -0800

    change dashboard path from /dashboard to app/dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Redirect to subdomain dashboard after sign-in
+  def after_sign_in_path_for(resource)
+    # Replace `subdomain` with the attribute that stores the user's subdomain
+    app_dashboard_url(subdomain: resource.subdomain)
+  end
 end
